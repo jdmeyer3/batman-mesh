2572b072a4f73ec975aa05887f495f0880bab0db
Joshua Meyer <jmeyer@charter.com>
2017-06-05 14:21:52 -0600

    create objects

--- batgwnode.py
+++ batgwnode.py
@@ -9,51 +9,56 @@ import os
 import sys
 import apt
 
-pkg_name = "batctl"
-wlan = "wlan0"
-eth = "ens33"
-bridgeip = "192.168.2.1"
-essid = "my-mesh-network"
-ap = "02:12:34:56:78:90"
+class batgwnode(object):
+
+    def __init__(self):
+        self.pkg_name = "batctl"
+        self.wlan = "wlan0"
+        self.eth = "ens33"
+        self.bridgeip = "192.168.2.1"
+        self.essid = "my-mesh-network"
+        self.ap = "02:12:34:56:78:90"
+
+    def installbat(self):
+        if not os.geteuid() == 0:
+            sys.exit("\nOnly root can run this script\n")
+
+        cache = apt.cache.Cache
+        cache.update()
+        pkg = cache[self.pkg_name]
+        if pkg.is_installed:
+            print "{pkg_name} already installed".format(pkg_name=self.pkg_name)
+        else:
+            pkg.mark_install()
+
+            try:
+                cache.commit()
+            except Exception, arg:
+                print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
 
 
-if not os.geteuid() == 0:
-    sys.exit("\nOnly root can run this script\n")
-
-cache = apt.cache.Cache
-cache.update()
-pkg = cache[pkg_name]
-if pkg.is_installed:
-    print "{pkg_name} already installed".format(pkg_name=pkg_name)
-else:
-    pkg.mark_install()
-
-    try:
-        cache.commit()
-    except Exception, arg:
-        print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
-
-# enable batman-adv module
-os.system("modprobe batman-adv")
+    def createbatgw(self):
+        # enable batman-adv module
+        os.system("modprobe batman-adv")
 
 
-# enable batman node
-# configure network
-os.system("sudo ifconfig {wlan0} mtu 1528".format(wlan0=wlan))
-os.system("ifconfig {wlan0} down".format(wlan0=wlan))
-os.wait(3)
-os.system("iwconfig {wlan0} mode ad-hoc essid {essid} ap {ap} channel 1".format(wlan0=wlan, ap=ap, essid=essid))
-os.system("sudo batctl if add {wlan0}".format(wlan0=wlan))
-os.system("ifconfig {wlan0} up".format(wlan0=wlan))
-os.system("ifconfig bat0 up")
+        # enable batman node
+        # configure network
+        os.system("sudo ifconfig {wlan0} mtu 1528".format(wlan0=self.wlan))
+        os.system("ifconfig {wlan0} down".format(wlan0=self.wlan))
+        os.wait(3)
+        os.system("iwconfig {wlan0} mode ad-hoc essid {essid} ap {ap} channel 1".format(wlan0=self.wlan, ap=self.ap, essid=self.essid))
+        os.system("sudo batctl if add {wlan0}".format(wlan0=self.wlan))
+        os.system("ifconfig {wlan0} up".format(wlan0=self.wlan))
+        os.system("ifconfig bat0 up")
 
-# creates mesh bridge to internet
-os.system("ip link add name mesh-bridge type bridge")
-os.system("ifconfig {eth0} down".format(eth0=eth))
-os.wait(3)
-os.system("ip link set dev {eth0} master mesh-bridge".format(eth0=eth))
-os.system("ip link set dev bat0 master mesh-bridge")
-os.system("ip link set up dev {eth0}".format(eth0=eth))
-os.system("ip link set up dev bat0")
-os.system("ip link set up dev mesh-bridge")
-os.system("ifconfig mesh-bridge {ipaddr}".format(ipaddr=bridgeip))
+        # creates mesh bridge to internet
+        os.system("ip link add name mesh-bridge type bridge")
+        os.system("ifconfig {eth0} down".format(eth0=self.eth))
+        os.wait(3)
+        os.system("ip link set dev {eth0} master mesh-bridge".format(eth0=self.eth))
+        os.system("ip link set dev bat0 master mesh-bridge")
+        os.system("ip link set up dev {eth0}".format(eth0=self.eth))
+        os.system("ip link set up dev bat0")
+        os.system("ip link set up dev mesh-bridge")
+        os.system("ifconfig mesh-bridge {ipaddr}".format(ipaddr=self.bridgeip))
--- batnode.py
+++ batnode.py
@@ -9,35 +9,46 @@ import os
 import sys
 import apt
 
-if not os.geteuid() == 0:
-    sys.exit("\nOnly root can run this script\n")
+class batnode(object):
 
-pkg_name = "batctl"
-wlan = "wlan0"
-cache = apt.cache.Cache
-cache.update()
-
-pkg = cache[pkg_name]
-if pkg.is_installed:
-    print "{pkg_name} already installed".format(pkg_name=pkg_name)
-else:
-    pkg.mark_install()
-
-    try:
-        cache.commit()
-    except Exception, arg:
-        print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
+    def __init__(self):
+        self.pkg_name = "batctl"
+        self.wlan = "wlan0"
+        self.essid = "bat-mesh-network"
+        self.ap = "02:12:34:56:78:90"
 
 
+    def batinstall(self):
+        if not os.geteuid() == 0:
+            sys.exit("\nOnly root can run this script\n")
 
-#enable batman-adv module
-os.system("modprobe batman-adv")
+        cache = apt.cache.Cache
+        cache.update()
+        pkg = cache[self.pkg_name]
+        if pkg.is_installed:
+            print "{pkg_name} already installed".format(pkg_name=self.pkg_name)
+        else:
+            pkg.mark_install()
+
+            try:
+                cache.commit()
+            except Exception, arg:
+                print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
 
 
-#configure network
-os.system("sudo ifconfig {wlan0} mtu 1528".format(wlan0=wlan))
-os.system("ifconfig {wlan0} down".format(wlan0=wlan))
-os.system("iwconfig {wlan0} mode ad-hoc essid my-mesh-network channel 1".format(wlan0=wlan))
-os.system("sudo batctl if add {wlan0}".format(wlan0=wlan))
-os.system("ifconfig {wlan0} up".format(wlan0=wlan))
-os.system("ifconfig bat0 up")
+    def createbatnode(self):
+        #enable batman-adv module
+        os.system("modprobe batman-adv")
+
+        #configure network
+        os.system("sudo ifconfig {wlan0} mtu 1528".format(wlan0=self.wlan))
+        os.system("ifconfig {wlan0} down".format(wlan0=self.wlan))
+        os.system("iwconfig {wlan0} mode ad-hoc essid {essid} ap {ap} channel 1".format(wlan0=self.wlan, ap=self.ap, essid=self.essid))
+        os.system("sudo batctl if add {wlan0}".format(wlan0=self.wlan))
+        os.system("ifconfig {wlan0} up".format(wlan0=self.wlan))
+        os.system("ifconfig bat0 up")
+
+if __name__ == "__main__":
+    newbat = batnode()
+    batnode.batinstall()
+    batnode.createbatnode()
